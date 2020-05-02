6b06fecbc99f03141394747ed7277e510e039cd0
Sandra Llanos <sllanos@bancolombia.com.co>
2020-05-01 20:32:36 -0500

    split children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
+
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
 
   {
     path:'usuario/:10', component:UsuarioComponent,
-    children:[
-      {path:'nuevo', component:UsuarioNuevoComponent},
-      {path:'editar', component:UsuarioEditarComponent},
-      {path:'detalles', component:UsuarioDetalleComponent},
-      {path:'**', pathMatch:'full', redirectTo:'nuevo'}
-    ]
+    children: USUARIO_ROUTES
   },
 
   {path:'**', pathMatch:'full', redirectTo:'home'},
